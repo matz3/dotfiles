86f201d33e2e352952be7794388e1d682917e887
Matthias Osswald <m@tthiasosswald.com>
2017-01-23 18:40:33 +0100

    zsh: add git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,7 @@ alias ni='npm install'
 alias nig='npm install --global'
 alias nid='npm install --save-dev'
 alias nis='npm install --save'
+
+alias such='git'
+alias very='git'
+alias wow='git status -sb'
