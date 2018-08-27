0ba6dbf747a80d7a04ade8831271cf264428c9ee
Matthias Osswald <m@tthiasosswald.com>
2018-08-27 19:44:46 +0200

    ruby: remove

--- ruby/env.zsh
+++
@@ -1,4 +0,0 @@
-export RBENV_ROOT="$HOME/.homebrew/opt/rbenv"
-export GEM_HOME="$HOME/.homebrew/opt/gems"
-export GEM_PATH="$GEM_HOME"
-export PATH="$HOME/.rbenv/bin:$GEM_HOME/bin:$PATH"
--- ruby/init.zsh
+++
@@ -1,4 +0,0 @@
-if which rbenv > /dev/null;
-	# Silently rehash in the background to improve startup performance (https://github.com/carsomyr/rbenv-bundler/issues/33#issuecomment-14334906)
-	then eval "$(rbenv init --no-rehash -)"; (rbenv rehash &) 2> /dev/null;
-fi
