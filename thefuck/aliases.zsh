1646b31e005bda66c44b7429b8b82df29f700704
Matthias Osswald <m@tthiasosswald.com>
2018-08-27 19:43:32 +0200

    thefuck: remove

--- thefuck/aliases.zsh
+++
@@ -1,3 +0,0 @@
-# eval $(thefuck --alias)
-# Replaced eval command with result for better startup performance
-alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD && print -s $TF_CMD'
