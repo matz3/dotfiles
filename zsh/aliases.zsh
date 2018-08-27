f91e6821d6bf21cc92e7aa5438c0370944ed804e
Matthias Osswald <m@tthiasosswald.com>
2018-08-27 19:41:37 +0200

    zsh: remove aliases (not used)

--- zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias reload!='. ~/.zshrc'
-alias ni='npm install'
-alias nig='npm install --global'
-alias nid='npm install --save-dev'
-alias nis='npm install --save'
-
-alias such='git'
-alias very='git'
-alias wow='git status -sb'
