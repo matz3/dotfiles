8d327c2bf068b396f0f3ccf17c3bbf7fdbbcf022
Matthias Osswald <m@tthiasosswald.com>
2018-08-27 19:30:38 +0200

    zsh: add autosuggestions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -7,3 +7,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
