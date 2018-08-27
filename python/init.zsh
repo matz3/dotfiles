83f2e7d99cd5557a51bf7d5504af8a049b973b1b
Matthias Osswald <m@tthiasosswald.com>
2018-08-27 19:47:53 +0200

    python: remove

--- python/init.zsh
+++
@@ -1,4 +0,0 @@
-if which pyenv > /dev/null;
-	# Silently rehash in the background to improve startup performance (https://github.com/carsomyr/rbenv-bundler/issues/33#issuecomment-14334906)
-	then eval "$(pyenv init --no-rehash -)"; (rbenv rehash &) 2> /dev/null;
-fi
