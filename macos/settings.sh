238f111c471bfe92dbc0f58942b5656e81d2a958
Matthias Osswald <mat.osswald@sap.com>
2019-08-01 08:31:09 +0200

    macos: Fix setting the default user shell

--- macos/settings.sh
+++ macos/settings.sh
@@ -8,8 +8,11 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Default user login shell
+# Default sudo user shell
 sudo chsh -s /bin/zsh
+
+# Default user shell
+chsh -s /bin/zsh
 
 ###############################################################################
 # General UI/UX                                                               #
