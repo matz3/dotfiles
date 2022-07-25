a7e4ee45241648aed2278d7735d09171e0e1d162
Matthias Osswald <mat.osswald@sap.com>
2022-07-25 15:34:18 +0200

    nvm: Add auto completion

--- nvm/init.zsh
+++ nvm/init.zsh
@@ -1,2 +1,3 @@
 export NVM_DIR="$NVM_HOME/.nvm" # NVM_HOME defined in zsh/localrc
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
